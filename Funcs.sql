ecf2f477920eddeb324c961edb5892da8d167616
Artur Amorim <62303570+duarteartur@users.noreply.github.com>
2022-12-01 16:04:35 -0300

    Funções em PostgreSQL

---
+++ Funcs.sql
@@ -0,0 +1,23 @@
+CREATE or replace FUNCTION MascaraCPF() returns text as $$
+DECLARE
+result RECORD;
+var_saida text;
+
+BEGIN
+
+RAISE INFO 'CNPJ';
+var_saida := 'Finalizado';
+
+FOR result IN select nr_controle,substr(nr_doc_cpf, 1, 3) || '.' ||
+       substr(nr_doc_cpf, 4, 3) || '.' ||
+       substr(nr_doc_cpf, 7, 3) || '-' ||
+       substr(nr_doc_cpf, 10) as formatado_cpf from pessoa_doc where length(nr_doc_cpf) = 11
+LOOP
+
+update pessoa_doc set nr_doc_cpf = result.formatado_cpf where nr_controle = result.nr_controle;
+
+
+END LOOP;
+return var_saida;
+END;
+$$ language 'plpgsql';
